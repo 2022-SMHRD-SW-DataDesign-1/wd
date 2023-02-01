8de16064fd1b072566fcee29b3dda099acca91ce
smhrd <smhrd@DESKTOP-H3NQ08I>
2023-02-01 17:17:26 +0900

    2023-02-01 이미지 업로드 완료_김지수

--- src/main/java/com/weed/wws/TestController.java
+++ src/main/java/com/weed/wws/TestController.java
@@ -1,37 +1,48 @@
 package com.weed.wws;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.weed.entity.WeedDTO;
 import com.weed.mapper.WwsMapper;
 
+import lombok.extern.log4j.Log4j;
+
 @Controller
+@Log4j
 public class TestController {
 
 	@Autowired
 	private WwsMapper wwsMapper;
 
 	@PostMapping(value = "/imgSave.do")
-	public String imgSave(@RequestParam("file") MultipartFile file,HttpServletRequest request) {
+	public String imgSave(MultipartFile[] uploadFile, Model model) {
 		
-		String fileRealName = file.getOriginalFilename();
+		System.out.println("[TestController]");
 		
-		System.out.println("파일명 : "+fileRealName);
+		String uploadFolder = "C:\\Users\\smhrd\\git\\wd\\src\\main\\webapp\\resources\\images\\testimg";
 		
-		String fileExtension = fileRealName.substring(fileRealName.lastIndexOf("."),fileRealName.length());
-		String uploadFolder = "resource\\images\\test";
-		
-		System.out.println(uploadFolder);
+		for(MultipartFile multipartFile : uploadFile) {
+			log.info("------------------------------------------");
+			log.info("Upload File Name: "+multipartFile.getOriginalFilename());
+			log.info("Upload File Size: "+multipartFile.getSize());
+			
+			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			
+			try {
+				multipartFile.transferTo(saveFile);
+				System.out.println("이미지 저장 완료");
+			} catch (Exception e) {
+				log.error(e.getMessage());
+				System.out.println("error");
+			} // end catch
+		} //end for
 		
 		return "test";
-		
-		}
+	}
 }

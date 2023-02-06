0dc78b5f290490d046b9b9491dc0302bbb4f8ca1
smhrd <smhrd@DESKTOP-OHN064C>
2023-02-07 08:55:16 +0900

    2023-02-07 소켓전송_김주희

--- src/main/java/com/weed/wws/SocketController.java
+++ src/main/java/com/weed/wws/SocketController.java
@@ -1,20 +1,28 @@
 package com.weed.wws;
 
-import java.net.Socket;
-import java.nio.charset.StandardCharsets;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Base64;
 
 import javax.imageio.ImageIO;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.xml.bind.DatatypeConverter;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -40,27 +48,40 @@ public class SocketController {
 			Socket soc = new Socket("localhost", 12345);
 			DataOutputStream dout = new DataOutputStream(soc.getOutputStream());
 			DataInputStream in = new DataInputStream(soc.getInputStream());
-			String msg = (String) in.readUTF();
 			
-			System.out.println("Server: " + msg);
-			
+			// 내가 가지고있는 값
 			String image = request.getParameter("image");
 			System.out.println(image);
 
 			String email = request.getParameter("email");
 			System.out.println(email);
-
+			// 이미지 확장자
 			int size = image.length();
 			String file = image.substring(size - 3);
-			System.out.println(file);
+			System.out.println("확장자:" + file);
 			
+			if(file!="jpg") {
+				image.replace(file, "jpg");
+			}
+			
+			// 이미지 byte값으로 변환 및 보내기
 			BufferedImage bImage = ImageIO.read(new File(image));
 			ByteArrayOutputStream bos = new ByteArrayOutputStream();
 			ImageIO.write(bImage,file,bos);
 			byte[] data = bos.toByteArray();
 			dout.write(data);
 			
-			dout.flush();
+			System.out.println("전송완료");
+			
+			// 파이썬에서 보내는 값
+			System.out.println("값 받아오기");
+			in.readFully(data);
+			
+			ByteArrayInputStream input_stream= new ByteArrayInputStream(data);
+			BufferedImage p_image = ImageIO.read(input_stream);
+			ImageIO.write(p_image , "jpg", new File("C:\\Users\\smhrd\\Desktop\\image2\\img.jpg"));
+			
+			// dout.flush();
 			dout.close();
 			soc.close();
 			System.out.println("닫힘");
